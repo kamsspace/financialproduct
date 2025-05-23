79d83f2384ec529721055ec01f4186bb4f80aa53
kamsspace <h.u.kamolov@icloud.com>
2025-05-23 14:30:04 +0500

    setup financialproduct project

---
+++ src/main/java/com/kamsspace/financialproduct/FinancialproductApplication.java
@@ -0,0 +1,13 @@
+package com.kamsspace.financialproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialproductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamsspace/financialproduct/FinancialproductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamsspace.financialproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialproductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
