555ed8c3821f0c3c24932d716d74a74e9e799235
kamsspace <h.u.kamolov@icloud.com>
2025-05-23 17:55:44 +0500

    Added the logic to createTransaction method

--- src/main/java/com/kamsspace/financialproduct/service/UserServiceImpl.java
+++ src/main/java/com/kamsspace/financialproduct/service/UserServiceImpl.java
@@ -1,8 +1,57 @@
 package com.kamsspace.financialproduct.service;
 
+import com.kamsspace.financialproduct.model.User;
+import com.kamsspace.financialproduct.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void createUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public String deleteUser(Long userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User Not Found"));
+
+        userRepository.delete(user);
+        return "User with userId " + userId + " deleted successfully";
+    }
+
+    @Override
+    public User updateUser(User user, Long userId) {
+
+        User savedUser = userRepository.findById(userId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User Not Found"));
+        user.setUserId(userId);
+        savedUser = userRepository.save(user);
+
+        return savedUser;
+    }
+
+    @Override
+    public User getUserById(Long userId) {
+
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User Not Found"));
+    }
+
 
 }
