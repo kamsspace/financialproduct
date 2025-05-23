430ab7ad113f6e55744dead0fd1aee32b75fd0be
kamsspace <h.u.kamolov@icloud.com>
2025-05-23 15:38:40 +0500

    Added RESTful API controller layers for UserController and TransactionController

--- src/main/java/com/kamsspace/financialproduct/controller/TransactionController.java
+++ src/main/java/com/kamsspace/financialproduct/controller/TransactionController.java
@@ -1,15 +1,45 @@
 package com.kamsspace.financialproduct.controller;
 
+import com.kamsspace.financialproduct.model.Transaction;
+import com.kamsspace.financialproduct.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/public/transactions")
 public class TransactionController {
 
     @Autowired
     private TransactionService transactionService;
 
+    @GetMapping("/public/transactions")
+    public ResponseEntity<List<Transaction>> getAllTransactions() {
+        List<Transaction> transactions = transactionService.getAllTransactions();
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
+
+    @PostMapping("/public/transactions")
+    public ResponseEntity<String> createTransaction(@RequestBody Transaction transaction) {
+        transactionService.createTransaction(transaction);
+        return new ResponseEntity<>("Transaction added successfully", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Transaction>> getTransactionsByUser(@PathVariable Long userId) {
+        List<Transaction> transactions = transactionService.getTransactionsByUser(userId);
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
+
+    @GetMapping("date-range")
+    public ResponseEntity<List<Transaction>> getTransactionsByDateRange(@RequestParam("startDate")LocalDateTime startDate,
+                                                                        @RequestParam("endDate") LocalDateTime endDate) {
+        List<Transaction> transactions = transactionService.getTransactionsByDateRange(startDate, endDate);
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/kamsspace/financialproduct/controller/UserController.java
+++ src/main/java/com/kamsspace/financialproduct/controller/UserController.java
@@ -1,4 +1,52 @@
 package com.kamsspace.financialproduct.controller;
 
+import com.kamsspace.financialproduct.model.User;
+import com.kamsspace.financialproduct.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/public/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @PostMapping("/public/users")
+    public ResponseEntity<String> createUsers(@RequestBody User user) {
+        userService.createUser(user);
+        return new ResponseEntity<>("User added successfully", HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/admin/users/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long userId) {
+        try {
+            String status = userService.deleteUser(userId);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
+    }
+
+    @PutMapping("/public/users/{userId}")
+    public ResponseEntity<String> updateUser(@RequestBody User user, @PathVariable Long userId) {
+        try {
+            User savedUser = userService.updateUser(user, userId);
+            return new ResponseEntity<>("User with userId: " + userId, HttpStatus.OK);
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
+    }
+
 }
