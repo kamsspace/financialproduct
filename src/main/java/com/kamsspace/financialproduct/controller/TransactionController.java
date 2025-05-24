1852d6a047e4511c42e06cb6412f3d497ffdd69f
kamsspace <h.u.kamolov@icloud.com>
2025-05-24 22:05:39 +0500

    Added validation annotations to User entity and added @Valid to TransactionController

--- src/main/java/com/kamsspace/financialproduct/controller/TransactionController.java
+++ src/main/java/com/kamsspace/financialproduct/controller/TransactionController.java
@@ -4,7 +4,6 @@ import com.kamsspace.financialproduct.model.Transaction;
 import com.kamsspace.financialproduct.service.TransactionService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/kamsspace/financialproduct/model/User.java
+++ src/main/java/com/kamsspace/financialproduct/model/User.java
@@ -4,13 +4,11 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.rmi.MarshalException;
 import java.util.List;
 
 @Entity
@@ -25,11 +23,11 @@ public class User {
     private Long userId;
 
     @NotBlank
-    @Size(min = 5, message = "Last name must at least be 5 characters")
+    @Size(min = 5, max = 30, message = "Last name must at least be 5 characters")
     private String userFirstName;
 
     @NotBlank
-    @Size(min = 5, message = "Last name must at least be 5 characters")
+    @Size(min = 5, max = 30, message = "Last name must at least be 5 characters")
     private String userLastName;
 
     @NotNull(message = "Balance must not be empty")
